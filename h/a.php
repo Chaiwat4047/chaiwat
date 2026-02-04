a703644be15589cda09ec141747436e7d8ba1b83
chaiwat4047 <66010914047@msu.ac.th>
2026-02-04 15:22:42 +0700

    a

--- h/a.php
+++ h/a.php
@@ -1,1 +1,25 @@
+<?php
+	session_start();
+?>
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>ไชยวัฒน์ เหล่าหลำ(เจย์)</title>
+</head>
+
+<body>
 <h1>ไชยวัฒน์ เหล่าหลำ(เจย์)</h1>
+
+<?php
+	$_SESSION['name'] ="ไชยวัฒน์ เหล่าหลำ";
+	$_SESSION['nickname'] ="เจย์";
+	$_SESSION['p1'] ="โซฟา";
+	$_SESSION['p2'] ="ห่วงยาง";
+?>
+
+
+
+
+</body>
+</html>
---
+++ h/e.php
@@ -0,0 +1,23 @@
+<?php
+	session_start();
+?>
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>ไชยวัฒน์ เหล่าหลำ(เจย์)</title>
+</head>
+
+<body>
+<h1>e.php</h1>
+
+<?php
+	unset(  $_SESSION['name'] ) ;
+	unset(  $_SESSION['p1'] ) ;
+?>
+
+
+
+
+</body>
+</html>
